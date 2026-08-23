b453279c402a502536ee76db976c6b1b479f7afd
zephtzk <zephtang@gmail.com>
2026-08-23 16:32:09 +0800

    greet user and exit

--- src/main/java/Chillguy.java
+++ src/main/java/Chillguy.java
@@ -1,10 +1,18 @@
 public class Chillguy {
     public static void main(String[] args) {
+        String separator = "_".repeat(60);
         String banner = "   _____ _   _ ___ _     _      _____ _   _ __   __\n"
                 + "  / ____| | | |_ _| |   | |    / ____| | | |\\ \\ / /\n"
                 + " | |    | |_| || || |   | |   | |  __| | | | \\ V /\n"
                 + " | |___ |  _  || || |___| |___| | |_ | |_| |  | |\n"
-                + "  \\____||_| |_|___|_____|______\\_____|____/   |_|\n";
+                + "  \\____||_| |_|___|_____|______\\_____|____/   |_|";
+
+        System.out.println(separator);
         System.out.println(banner);
+        System.out.println("Hello! I'm Chillguy.");
+        System.out.println("What can I do for you?");
+        System.out.println(separator);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(separator);
     }
 }
