009594f3137fa747d1852f3a1f57773390e90f9b
zephtzk <zephtang@gmail.com>
2026-08-31 17:56:05 +0800

    Add in-memory task listing

--- src/main/java/Chillguy.java
+++ src/main/java/Chillguy.java
@@ -4,6 +4,14 @@ import java.util.Scanner;
  * Runs the Chillguy chatbot.
  */
 public class Chillguy {
+    private static final int MAX_TASKS = 100;
+
+    /**
+     * Starts the chatbot and processes commands until the user enters {@code bye}.
+     * Ordinary text is stored as a task, while {@code list} displays all stored tasks.
+     *
+     * @param args command-line arguments, which are not used
+     */
     public static void main(String[] args) {
         String separator = "_".repeat(60);
         String banner = "   _____ _   _ ___ _     _      _____ _   _ __   __\n"
@@ -19,8 +27,12 @@ public class Chillguy {
         System.out.println(separator);
 
         Scanner scanner = new Scanner(System.in);
+        String[] tasks = new String[MAX_TASKS];
+        int taskCount = 0;
+
         while (scanner.hasNextLine()) {
             String command = scanner.nextLine();
+            System.out.println(separator);
 
             if (command.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
@@ -28,7 +40,16 @@ public class Chillguy {
                 break;
             }
 
-            System.out.println(command);
+            if (command.equals("list")) {
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println((i + 1) + ". " + tasks[i]);
+                }
+            } else {
+                tasks[taskCount] = command;
+                taskCount++;
+                System.out.println("added: " + command);
+            }
+
             System.out.println(separator);
         }
     }
